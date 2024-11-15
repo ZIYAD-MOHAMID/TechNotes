570387aaf4047c3f9b8459a363e5d7a3efa78661
Your Name <ziyadsha44@gmail.com>
2024-11-15 02:15:25 +0200

    React Forms with Redux & RTK Query

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,11 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Public from './components/Public'
-import Login from './features/auth/Login';
-import DashLayout from './components/DashLayout'
-import Welcome from './features/auth/Welcome'
-import NotesList from './features/notes/NotesList'
-import UsersList from './features/users/UsersList'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+import NewUserForm from "./features/users/NewUserForm";
+import NewNote from "./features/notes/NewNote";
+import EditNote from "./features/notes/EditNote";
+import EditUser from "./features/users/EditUser";
+import Prefetch from "./features/auth/Prefetch";
 
 function App() {
   return (
@@ -14,20 +19,23 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route path="dash" element={<DashLayout />}>
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
 
-          <Route index element={<Welcome />} />
-
-          <Route path="notes">
-            <Route index element={<NotesList />} />
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
           </Route>
-
-          <Route path="users">
-            <Route index element={<UsersList />} />
-          </Route>
-
-        </Route>{/* End Dash */}
-
+        </Route>
+        {/* End Dash */}
       </Route>
     </Routes>
   );
