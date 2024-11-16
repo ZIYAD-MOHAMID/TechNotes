536cb13017fc5dff0423070c0cd650c57c437d4e
Your Name <ziyadsha44@gmail.com>
2024-11-16 06:31:43 +0200

    Backend

---
+++ backEnd/routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController.js");
+const loginLimiter = require("../middleware/loginLimiter.js");
+
+router.route("/").post(loginLimiter, authController.login);
+router.route("/refresh").get(authController.refresh);
+router.route("/logout").post(authController.logout);
+
+module.exports = router;
